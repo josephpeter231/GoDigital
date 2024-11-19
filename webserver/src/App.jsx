de2508ff28648ca366987f5bbe35bb3fa04fbed8
josephpeter231 <josephpeterjcit@gmail.com>
2024-11-19 06:50:58 +0530

    main

--- webserver/src/App.jsx
+++ webserver/src/App.jsx
@@ -1,22 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login.jsx";
 import Register from "./pages/Register.jsx";
 
 function App() {
   return (
-    <Router
-      future={{
-        v7_startTransition: true, // Enables React.startTransition for state updates
-        v7_relativeSplatPath: true, // Updates relative splat path resolution
-      }}
-    >
+    <BrowserRouter>
       <div className="min-h-screen flex justify-center items-center bg-gray-100">
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
